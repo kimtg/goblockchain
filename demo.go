3531b04caba233058800877cf068f6e6e6adeb58
KIM Taegyoon <steloflute@gmail.com>
2018-06-21 23:54:41 +0900

    simplified

--- demo.go
+++ demo.go
@@ -7,7 +7,6 @@ import (
 )
 
 func main() {
-	var b goblockchain.Block
 	bc := goblockchain.NewBlockchain(5)
 	fmt.Println("Simple blockchain demo")
 	fmt.Println()
@@ -15,22 +14,20 @@ func main() {
 	fmt.Println("Mining first block...")
 	bc.MineBlock("First block")
 	fmt.Println("Block mined.")
-	b = bc.LastBlock()
-	fmt.Printf("Difficulty: %v, nonce: %v\n", bc.GetDifficulty(), b.GetNonce())
+	fmt.Println(bc.LastBlock())
 	fmt.Println()
 
 	fmt.Println("Mining second block...")
 	bc.MineBlock("Second block")
 	fmt.Println("Block mined.")
-	b = bc.LastBlock()
-	fmt.Printf("Difficulty: %v, nonce: %v\n", bc.GetDifficulty(), b.GetNonce())
+	fmt.Println(bc.LastBlock())
 	fmt.Println()
 
 	fmt.Println("Mining third block...")
 	bc.MineBlock("Third block")
 	fmt.Println("Block mined.")
-	b = bc.LastBlock()
-	fmt.Printf("Difficulty: %v, nonce: %v\n", bc.GetDifficulty(), b.GetNonce())
+	fmt.Println(bc.LastBlock())
+
 	fmt.Println()
 
 	fmt.Println("Is blockchain valid? " + strconv.FormatBool(bc.IsValid()))
