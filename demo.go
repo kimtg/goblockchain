8de932223c6b9f009a3d258f8fc90f0e8396a486
KIM Taegyoon <steloflute@gmail.com>
2018-06-21 23:46:50 +0900

    added separating blank lines

--- demo.go
+++ demo.go
@@ -10,26 +10,31 @@ func main() {
 	var b goblockchain.Block
 	bc := goblockchain.NewBlockchain(5)
 	fmt.Println("Simple blockchain demo")
+	fmt.Println()
 
 	fmt.Println("Mining first block...")
 	bc.MineBlock("First block")
 	fmt.Println("Block mined.")
 	b = bc.LastBlock()
 	fmt.Printf("Difficulty: %v, nonce: %v\n", bc.GetDifficulty(), b.GetNonce())
+	fmt.Println()
 
 	fmt.Println("Mining second block...")
 	bc.MineBlock("Second block")
 	fmt.Println("Block mined.")
 	b = bc.LastBlock()
 	fmt.Printf("Difficulty: %v, nonce: %v\n", bc.GetDifficulty(), b.GetNonce())
+	fmt.Println()
 
 	fmt.Println("Mining third block...")
 	bc.MineBlock("Third block")
 	fmt.Println("Block mined.")
 	b = bc.LastBlock()
 	fmt.Printf("Difficulty: %v, nonce: %v\n", bc.GetDifficulty(), b.GetNonce())
+	fmt.Println()
 
 	fmt.Println("Is blockchain valid? " + strconv.FormatBool(bc.IsValid()))
+	fmt.Println()
 
 	fmt.Println("Printing blockchain...");
 	fmt.Println(bc)
